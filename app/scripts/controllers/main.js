8dd3db21972150ba84f666a9df859336da8f9fca
Benjamin Schuch <bschuch@gmx.net>
2015-05-08 22:55:49 +0200

    Reinstalled without SASS / Compass in favour of LESS

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name deviceRegistrationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the deviceRegistrationApp
+ */
+angular.module('deviceRegistrationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
