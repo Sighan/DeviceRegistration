4f0edc4cbc33a77f85d562e38bd2849f8ff8900c
dan <daniel.rockenstein@thekeytechnlogies.de>
2015-05-19 14:43:48 +0200

    Added http interceptor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,7 +21,7 @@ angular
   ]).constant('urls', {
         API: 'http://<hier api domain eingeben>'
     })
-    .config(function($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
         //
         // For any unmatched url, redirect to /state1
         $urlRouterProvider.otherwise('/login');
@@ -45,4 +45,22 @@ angular
                 url: '/forgotpwd',
                 templateUrl: 'views/login/forgotpwd.html'
             });
+
+        $httpProvider.interceptors.push(['$q', '$location', '$localStorage', function ($q, $location, $localStorage) {
+           return {
+               'request': function (config) {
+                   config.headers = config.headers || {};
+                   if ($localStorage.token) {
+                       config.headers.Authorization = 'Bearer ' + $localStorage.token;
+                   }
+                   return config;
+               },
+               'responseError': function (response) {
+                   if (response.status === 401 || response.status === 403) {
+                       $location.path('/login');
+                   }
+                   return $q.reject(response);
+               }
+           };
+        }]);
     });
