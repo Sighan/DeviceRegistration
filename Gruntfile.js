2ac18ff193465e6656ed2af5cb6ad01e5966274f
Benjamin Schuch <bschuch@gmx.net>
2015-05-28 02:40:34 +0200

    Added .less files to livereload watcher

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,8 +45,8 @@ module.exports = function (grunt) {
                 tasks: ['newer:jshint:test', 'karma']
             },
             styles: {
-                files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-                tasks: ['newer:copy:styles', 'autoprefixer']
+                files: ['<%= yeoman.app %>/styles/{,*/}*.css', '<%= yeoman.app %>/styles/less/{,*/}*.less'],
+                tasks: ['less', 'newer:copy:styles', 'autoprefixer']
             },
             gruntfile: {
                 files: ['Gruntfile.js']
