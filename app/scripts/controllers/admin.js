e0f55c788b58cce95f76ae119290ab72437d3e4c
Benjamin Schuch <bschuch@gmx.net>
2015-07-06 04:00:31 +0200

    Small layout changes

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -10,17 +10,19 @@
 angular.module('deviceRegistrationApp')
     .controller('AdminCtrl', function ($scope, $interval, uuid4) {
         $scope.invite = function (user, isValid) {
-            $scope.invited = true;
-        }
+            if (isValid) {
+                $scope.invited = true;
+            }
+        };
 
         $scope.generate = function () {
             $scope.progress = 0;
             $interval(function () {
                 $scope.progress++;
-                if ($scope.progress == 5) {
+                if ($scope.progress === 5) {
                     $scope.token = uuid4.generate();
                     console.log($scope.token);
                 }
             }, 500, 5);
-        }
+        };
     });
